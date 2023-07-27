37e4631f56979fa54490f4979ffe98f8eb413cc9
mahmoud adel <mahmoudadel5556@gmail.com>
2023-07-27 16:01:13 +0300

    State driven UI
    - https://developer.android.com/codelabs/jetpack-compose-state#5

--- app/src/main/java/com/example/compose_application/MainActivity.kt
+++ app/src/main/java/com/example/compose_application/MainActivity.kt
@@ -1,47 +1,76 @@
 package com.example.compose_application
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose_application.ui.theme.Compose_ApplicationTheme
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Compose_ApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    RecipeList()
-                }
+            ClickedOnButton()
+        }
+    }
+
+
+}
+
+
+
+@Composable
+@Preview(showBackground = true)
+private fun ClickedOnButton(){
+    var count by rememberSaveable { mutableStateOf(0)}
+    Compose_ApplicationTheme {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        )  {
+            Text(text = "you clicked $count times")
+            Button(onClick = {
+                count++
+            })
+            {
+                Text(text = "Add One")
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
+private fun CallRecipeList(){
     Compose_ApplicationTheme {
-//        Greeting("Android")
-        RecipeList()
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            RecipeList()
+        }
     }
 }
